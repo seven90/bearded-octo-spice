516a472d7b7ec94c8540e1033c6755afc2f8d8a5
Seven90 <stevesurya@gmail.com>
2014-06-19 16:18:12 -0400

    Created concept model

--- octo/db/schema.rb
+++ octo/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619194423) do
+ActiveRecord::Schema.define(version: 20140619201140) do
+
+  create_table "concepts", force: true do |t|
+    t.integer  "lesson_id"
+    t.text     "bullet"
+    t.integer  "counter"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "lessons", force: true do |t|
     t.integer  "teacher_id"
