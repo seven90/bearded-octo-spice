2c9a3c7fc8b52ebb1a216f71f1eccd10e71743c7
Ash Darji <ashishdarji88@gmail.com>
2014-06-20 10:10:13 -0400

    devise setup

--- octo/config/application.rb
+++ octo/config/application.rb
@@ -19,5 +19,8 @@ module Octo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    #For Deploying on Heroku - forces application to not access the DB or load models when precompiling assets
+    config.assets.initialize_on_precompile = false
   end
 end
--- octo/config/routes.rb
+++ octo/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 resources :teachers
+
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
